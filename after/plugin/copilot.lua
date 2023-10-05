16e7f307c41876c8f5d881c5454f6dd5dd4ec01b
Henrique Hirako <henriquehirako@gmail.com>
2023-10-05 16:31:08 -0700

    Custom Telescope find_files command

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,7 @@
+-- "Copilot"
+-- let g:copilot_no_tab_map = v:true
+-- let g:copilot_no_virtual_text = v:true
+-- inoremap <silent><script><expr> <C-Space> copilot#Accept("\<CR>")
+-- vim.cmd [[inoremap <silent><script><expr> <C-J> copilot#Next()]]
+-- vim.cmd [[inoremap <silent><script><expr> <C-K> copilot#Previous()]]
+-- vim.cmd [[nnoremap <silent><expr><C-@> ":Copilot panel<CR>"]]
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,8 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
+-- Show hidden files; respect .gitignore; exclude .git directory
+local custom_find_files = ':Telescope find_files hidden=true find_command={"rg","--files","--hidden","--follow","--glob","!.git/*"} <CR>'
+vim.keymap.set('n', '<C-p>', custom_find_files, {})
 vim.keymap.set('n', '<leager>g', builtin.git_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
@@ -7,8 +10,22 @@ vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 
 
 local actions = require('telescope.actions')
+
 require('telescope').setup({
   defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+      "--follow",
+      "--glob",
+      "!.git/*"
+    },
     mappings = {
       i = {
         ["<esc>"] = actions.close
