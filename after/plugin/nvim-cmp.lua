180cc218b209b7c6ba92294b8309d0984f215cea
Henrique Hirako <henriquehirako@gmail.com>
2025-03-27 08:39:15 -0700

    Tab and Enter behavior for nvim-cmp

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -2,49 +2,87 @@
 local cmp = require'cmp'
 local lspkind = require('lspkind')
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
   window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
+
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm({ select = true }),
+      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        if #cmp.get_entries() == 1 then
+          cmp.confirm({ select = true })
+        else
+          cmp.select_next_item()
+        end
+      --[[ Replace with your snippet engine (see above sections on this page)
+      elseif snippy.can_expand_or_advance() then
+        snippy.expand_or_advance() ]]
+      elseif has_words_before() then
+        cmp.complete()
+        if #cmp.get_entries() == 1 then
+          cmp.confirm({ select = true })
+        end
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
     { name = 'copilot' },
     { name = 'nvim_lsp' },
   }, {
     { name = 'buffer' },
-  })
+  }),
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(
-    {
-      ['<CR>'] = {
-        c = function(default)
-          if cmp.visible() then
-            return cmp.confirm({ select = true })
-          end
-
-          default()
-        end,
-      },
-      ['<Tab>'] = {
-        i = ...,
-        c = cmp.config.disable
-      }
-    }
-  ),
-  sources = {
-    { name = 'buffer' }
-  }
-})
+-- cmp.setup.cmdline({ '/', '?' }, {
+--   mapping = cmp.mapping.preset.cmdline(
+--     {
+--       ['<CR>'] = {
+--         c = function(default)
+--           if cmp.visible() then
+--             return cmp.confirm({ select = true })
+--           end
+--           default()
+--         end,
+--       },
+--       ['<Tab>'] = {
+--         i = ...,
+--         c = cmp.config.disable
+--       }
+--     }
+--   ),
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 -- cmp.setup.cmdline(':', {
@@ -89,8 +127,8 @@ cmp.setup {
         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
         -- can also be a function to dynamically calculate max width such as
         -- menu = function() return math.floor(0.45 * vim.o.columns) end,
-        menu = 50, -- leading text (labelDetails)
-        abbr = 50, -- actual suggestion item
+        menu = 100, -- leading text (labelDetails)
+        abbr = 100, -- actual suggestion item
       },
       ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
       show_labelDetails = true, -- show labelDetails in menu. Disabled by default
@@ -99,6 +137,8 @@ cmp.setup {
       -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
       before = function (entry, vim_item)
         -- ...
+        vim_item.menu = ""
+        vim_item.kind = ""
         return vim_item
       end
     })
